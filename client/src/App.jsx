3b7a0c26be48863b91243f70cf88ad30794989b0
mdirshaddev <mdirshaddev@gmail.com>
2021-04-11 17:04:22 +0530

    Changing the background color

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 export default function App(){
   return (
-    <div className="h-screen flex flex-col justify-center items-center bg-red-500">
+    <div className="h-screen flex flex-col justify-center items-center bg-green-500">
       <div class="chat-notification">
         <div class="chat-notification-logo-wrapper">
           <img class="chat-notification-logo" src="https://images.vexels.com/media/users/3/139911/isolated/preview/1afb4038427b2bd8edd275940aea269d-chat-service-icon-by-vexels.png" alt="ChitChat Logo" />
